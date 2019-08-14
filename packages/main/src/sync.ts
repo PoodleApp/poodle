c72b6cac9b6f4d192077d03b4154ddd47f7ba5f1
Jesse Hallett <jesse@sitr.us>
2019-08-14 14:35:12 -0400

    lots of debugging output for osx build

--- packages/main/src/sync.ts
+++ packages/main/src/sync.ts
@@ -39,6 +39,7 @@ export async function search(
   searchRecord: cache.Search,
   manager: ConnectionManager
 ) {
+  console.log("begin search", searchRecord)
   const accountId = searchRecord.account_id
   const boxRecord = cache.getSearchedBox(searchRecord)
   const box = await manager
@@ -95,6 +96,7 @@ class BoxSync {
   }
 
   async search(searchRecord: cache.Search) {
+    console.log("in search", searchRecord)
     const uids = List(
       await this.manager
         .request(
@@ -109,12 +111,16 @@ class BoxSync {
       .take(MAX_SEARCH_RESULTS)
       .map(uid => parseInt(uid, 10))
 
+    console.log("got uids", uids)
+
     // Add messages that have already been downloaded to result set
     cache.addSearchResults({
       search: searchRecord,
       uids,
       updatedAt: this.updatedAt
     })
+
+    console.log("added first search results")
 
     await this.downloadMissingMessages({
       uids,
@@ -127,7 +133,12 @@ class BoxSync {
       },
       batchSize: 1
     })
+
+    console.log("downloaded messages")
+
     cache.removeStaleSearchResults(searchRecord, this.updatedAt)
+
+    console.log("removed stale search results")
 
     // Get complete conversations
     for (const threadId of cache.getThreadIds({ uids })) {
@@ -139,8 +150,12 @@ class BoxSync {
       await this.downloadMissingMessages({ uids: threadUids })
     }
 
+    console.log("got complete conversations")
+
     // Make a record of the point when the search was fresh
     cache.setSearchUidLastSeen(searchRecord, this.box.uidnext - 1)
+
+    console.log("set search last seen")
 
     publishMessageUpdates(null)
   }
