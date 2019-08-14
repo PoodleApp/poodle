b0df8fab3ac98121cf4f583924bd29f695eb7488
Jesse Hallett <jesse@sitr.us>
2019-08-14 14:16:44 -0400

    try to catch rejection; install windows build tools

--- packages/main/jest.setup.js
+++ packages/main/jest.setup.js
@@ -1,12 +1,3 @@
 if (process.env.CI) {
   jest.setTimeout(120000)
 }
-
-// In Node v7 unhandled promise rejections will terminate the process
-if (!process.env.LISTENING_TO_UNHANDLED_REJECTION) {
-  process.on('unhandledRejection', reason => {
-    throw reason
-  })
-  // Avoid memory leak by adding too many listeners
-  process.env.LISTENING_TO_UNHANDLED_REJECTION = true
-}
