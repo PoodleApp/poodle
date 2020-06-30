b172716f44ebff4af3c000084a8d71c07c1948a7
Jesse Hallett <jesse@sitr.us>
2020-06-30 15:05:40 -0700

    update CSP to allow React error overlay in development mode

--- packages/client/config-overrides.js
+++ packages/client/config-overrides.js
@@ -16,7 +16,13 @@ const cspConfigPolicy = {
 const cspConfigPolicy_dev = {
   ...cspConfigPolicy,
   "connect-src": ["'self'", "ws://localhost:3000"],
-  "script-src": ["chrome-extension:"]
+  "script-src": [
+    "chrome-extension:",
+    // Hash of source for react overlay iframe which displays error information
+    // in development mode. It might be necessary to update this hash when
+    // changing React versions.
+    "'sha256-QAj9SgqS0tkqFXsMg6gbHzN3KfNnrPW0N0FCdMzN3MI='"
+  ]
 }
 
 function addCspHtmlWebpackPlugin(config) {
@@ -24,14 +30,16 @@ function addCspHtmlWebpackPlugin(config) {
     process.env.NODE_ENV === "production"
       ? cspConfigPolicy
       : cspConfigPolicy_dev
-  config.plugins.push(new cspHtmlWebpackPlugin(policy, {
-    nonceEnabled: {
-      // We need to disable nonces for styles because we want to use the
-      // `unsafe-inline` option, and if any nonces or hashes are included in
-      // a CSP policy then `unsafe-inline` is ignored.
-      'style-src': false
-    }
-  }))
+  config.plugins.push(
+    new cspHtmlWebpackPlugin(policy, {
+      nonceEnabled: {
+        // We need to disable nonces for styles because we want to use the
+        // `unsafe-inline` option, and if any nonces or hashes are included in
+        // a CSP policy then `unsafe-inline` is ignored.
+        "style-src": false
+      }
+    })
+  )
   return config
 }
 
